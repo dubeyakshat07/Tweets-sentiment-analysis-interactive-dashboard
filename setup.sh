2a511d36a7b880fb01f74220e7ddcb3b85a3a040
dubeyakshat07 <dubeyakshat07@gmail.com>
2020-09-25 18:59:23 +0530

    Fixed the runtime

--- setup.sh
+++
@@ -1,9 +0,0 @@
-mkdir -p ~/.streamlit/echo “\
-[general]\n\
-email = \”dubeyakshat07@gmail.com\”\n\
-“ > ~/.streamlit/credentials.tomlecho “\
-[server]\n\
-headless = true\n\
-enableCORS=false\n\
-port = $PORT\n\
-“ > ~/.streamlit/config.toml
